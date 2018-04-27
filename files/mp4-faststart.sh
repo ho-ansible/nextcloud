a044077dac30e7ac93dd76246426bb28e835a03d
Sean Ho <git@seanho.com>
2018-04-26 19:54:56 -0700

    search for both .mp4 and .m4v as video extensions

--- files/mp4-faststart.sh
+++ files/mp4-faststart.sh
@@ -2,5 +2,5 @@
 ## Find *.MP4 movies under the given dir,
 ## move QT MOOV atom to start for pseudo-streaming
 
-find $@ -xdev -iname '[^_]*.mp4' \
+find $@ -xdev -iname '[^_]*.m{p4,4v}' \
   -execdir qt-faststart '{}' '_{}' \;
