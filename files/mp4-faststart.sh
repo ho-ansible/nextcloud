5b4ded34b936d6b5675696ac8d4f0c0b9aeb5605
Sean Ho <git@seanho.com>
2018-04-27 16:22:58 -0700

    fix typo, missing '-' in option

--- files/mp4-faststart.sh
+++ files/mp4-faststart.sh
@@ -3,5 +3,8 @@
 ## move QT MOOV atom to start for pseudo-streaming
 
 find $@ -xdev \
-  \( -iname '[^_]*.mp4' -o iname '[^_]*.m4v' \) \
-  -execdir qt-faststart '{}' '_{}' \;
+  \( -iname '[^_]*.mp4' -o -iname '[^_]*.m4v' \) \
+  -execdir qt-faststart '{}' tmp \; \
+  -execdir touch -r '{}' tmp \; \
+  -execdir mv tmp '_{}' \; \
+  -delete
