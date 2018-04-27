af21db5ea179fd2925d018999e721c90b5315382
Sean Ho <git@seanho.com>
2018-04-27 16:23:58 -0700

    add note about qt-faststart failing

--- files/mp4-faststart.sh
+++ files/mp4-faststart.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 ## Find *.MP4 movies under the given dir,
 ## move QT MOOV atom to start for pseudo-streaming
+## TODO: what if MOOV atom is not at end (qt-faststart fails)?
 
 find $@ -xdev \
   \( -iname '[^_]*.mp4' -o -iname '[^_]*.m4v' \) \
